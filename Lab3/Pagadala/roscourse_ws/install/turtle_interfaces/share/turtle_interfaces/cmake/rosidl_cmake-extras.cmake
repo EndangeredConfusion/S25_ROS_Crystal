137ea497631dd6b215eff62c66930e18b508891a
Rica <pagadc@rpi.edu>
2025-02-05 03:45:43 +0800

    Lab 3 files

---
+++ Lab3/Pagadala/roscourse_ws/install/turtle_interfaces/share/turtle_interfaces/cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(turtle_interfaces_IDL_FILES "msg/Turtlemsg.idl;srv/Setpose.idl;srv/Setcolor.idl")
+set(turtle_interfaces_INTERFACE_FILES "msg/Turtlemsg.msg;srv/Setpose.srv;srv/Setpose_Request.msg;srv/Setpose_Response.msg;srv/Setcolor.srv;srv/Setcolor_Request.msg;srv/Setcolor_Response.msg")
